0cf2dcfb4f9ba82910ce9bf6df6dd18d9bbc8c4c
Elisa Capololo <elisacapololo2017@gmail.com>
2023-04-19 08:55:56 +0000

    main document organization

---
+++ lib/events_lagos_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class LagosPage extends StatefulWidget {
+  const LagosPage({super.key});
+
+
+  @override
+  State<LagosPage> createState() => _LagosPageState();
+}
+
+class _LagosPageState extends State<LagosPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: Center(
+       
+        child: Column(
+          
+        ),
+      ),
+    );
+  }
+}
